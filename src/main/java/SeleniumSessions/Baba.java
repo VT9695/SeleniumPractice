4d0a587006cbbb66b6320d2e3cf49c4b6a247bb5
vaibhav181818 <vaibhavsumbe1818@gmail.com>
2024-02-06 08:54:12 +0530

    modied baba file

--- src/main/java/SeleniumSessions/Baba.java
+++ src/main/java/SeleniumSessions/Baba.java
@@ -6,6 +6,22 @@ public class Baba {
  
 		int i=10;
 		System.out.println("i is"+i);
+		
+		BrowserUtil br = new BrowserUtil();
+		br.lauchBrowser("chrome");
+		br.enterUrl("https://www.amazon.com");
+		String title =	br.getPageTitle();
+		System.out.println("title is ...."+title);
+		
+		if(title.contains("Amazon.com. Spend less. Smile more.")) {
+			System.out.println("Matched -----Title");
+		}else
+			System.out.println("Title is not matched");
+		
+		String url=	br.getAppCurrentUrl();
+		System.out.println("the URL of the page is ----"+url);
+		br.quitBrowser();
+		//new line added
 	}
 
 }
