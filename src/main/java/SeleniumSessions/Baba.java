d62457baaec992efa7cf02ecc41053b2e8484aaa
vaibhav181818 <vaibhavsumbe1818@gmail.com>
2024-02-06 12:52:21 +0530

    removed the things which added in comment

--- src/main/java/SeleniumSessions/Baba.java
+++ src/main/java/SeleniumSessions/Baba.java
@@ -3,19 +3,14 @@ package SeleniumSessions;
 public class Baba {
 
 	public static void main(String[] args) {
-
 		BrowserUtil br = new BrowserUtil();
 		br.lauchBrowser("chrome");
 		br.enterUrl("https://www.amazon.com");
 		String title = br.getPageTitle();
 		System.out.println("title is ...." + title);
-
 		if (title.contains("Amazon.com. Spend less. Smile more.")) {
 			System.out.println("Matched -----Title");
 		} else
 			System.out.println("Title is not matched");
-
-		// new line added
 	}
-
 }
